cf4a45e81882b3cf5e3e9d8d8a1e7ecc3c1a957c
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-01-23 16:12:30 -0800

    remove controlled vocab tests; this is probably not core AF functionality

--- spec/unit/rdf_resource_spec.rb
+++ spec/unit/rdf_resource_spec.rb
@@ -214,44 +214,48 @@ describe ActiveFedora::Rdf::RdfResource do
     end
   end
 
-  describe 'controlled vocabularies' do
-    before(:each) do
-      RDF_VOCABS[:dcmitype] = { :prefix => 'http://purl.org/dc/dcmitype/', :source => 'http://dublincore.org/2012/06/14/dctype.rdf' }
-      RDF_VOCABS[:geonames] = { :prefix => 'http://sws.geonames.org/', :strict => false, :fetch => false }
-      class DummyStrict < Rdf::RdfResource
-        include Rdf::Controlled
-        use_vocabulary :dcmitype
-      end
-      class DummyVocab < Rdf::RdfResource
-        include Rdf::Controlled
-        use_vocabulary :geonames
-      end
-      DummyResource.property(:type, :predicate => RDF::DC.type, :class_name => DummyStrict)
-      DummyResource.property(:geo, :predicate => RDF::DC.spatial, :class_name => DummyVocab)
-      subject.type = DummyStrict.new('Image')
-      subject.geo = DummyVocab.new('http://sws.geonames.org/5735237/')
-    end
-    after(:each) do
-      Object.send(:remove_const, 'DummyStrict') if Object
-      Object.send(:remove_const, 'DummyVocab') if Object
-    end
+  # describe 'controlled vocabularies' do
+  #   before(:each) do
+  #     RDF_VOCABS[:dcmitype] = { :prefix => 'http://purl.org/dc/dcmitype/', :source => 'http://dublincore.org/2012/06/14/dctype.rdf' }
+  #     RDF_VOCABS[:geonames] = { :prefix => 'http://sws.geonames.org/', :strict => false, :fetch => false }
+  #     class DummyStrict < Rdf::RdfResource
+  #       include Rdf::Controlled
+  #       use_vocabulary :dcmitype
+  #     end
+  #     class DummyVocab < Rdf::RdfResource
+  #       include Rdf::Controlled
+  #       use_vocabulary :geonames
+  #     end
+  #     DummyResource.property(:type, :predicate => RDF::DC.type, :class_name => DummyStrict)
+  #     DummyResource.property(:geo, :predicate => RDF::DC.spatial, :class_name => DummyVocab)
+  #     subject.type = DummyStrict.new('Image')
+  #     subject.geo = DummyVocab.new('http://sws.geonames.org/5735237/')
+  #   end
+  #   after(:each) do
+  #     Object.send(:remove_const, 'DummyStrict') if Object
+  #     Object.send(:remove_const, 'DummyVocab') if Object
+  #   end
 
-    it 'should accept controlled terms' do
-      expect(subject.type.first).to be_kind_of DummyStrict
-    end
-    it 'should accept uncontrolled external terms' do
-      expect(subject.geo.first).to be_kind_of DummyVocab
-    end
-    it 'should load data for controlled terms' do
-      DummyStrict.load_vocabularies
-      subject.reload
-      expect(subject.type.first.has_subject?(RDF::URI('http://purl.org/dc/dcmitype/Image'))).to be_true
-    end
-    it 'should load data for uncontrolled external terms' do
-      subject.geo.first.fetch
-      expect(subject.geo.first.has_subject?(RDF::URI('http://sws.geonames.org/5735237/'))).to be_true
-    end
-  end
+  #   it 'should accept controlled terms' do
+  #     expect(subject.type.first).to be_kind_of DummyStrict
+  #   end
+  #   it 'should accept uncontrolled external terms' do
+  #     expect(subject.geo.first).to be_kind_of DummyVocab
+  #   end
+  #   it 'should load data for controlled terms' do
+  #     DummyStrict.load_vocabularies
+  #     subject.reload
+  #     expect(subject.type.first.has_subject?(RDF::URI('http://purl.org/dc/dcmitype/Image'))).to be_true
+  #   end
+  #   it 'should load data for uncontrolled external terms' do
+  #     subject.geo.first.fetch
+  #     expect(subject.geo.first.has_subject?(RDF::URI('http://sws.geonames.org/5735237/'))).to be_true
+  #   end
+  #   it 'should load data for uncontrolled external terms' do
+  #     subject.geo.first.fetch
+  #     expect(subject.geo.first.has_subject?(RDF::URI('http://sws.geonames.org/5735237/'))).to be_true
+  #   end
+  # end
 
   describe 'big complex graphs' do
     it 'should allow access to deep nodes'
