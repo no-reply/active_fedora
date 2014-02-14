26efdbd2290959e8bd4437cc51d96d1e59dd3b2d
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-02-14 15:41:55 -0800

    new rdf_list implementation

--- lib/active_fedora/rdf/rdf_list.rb
+++ lib/active_fedora/rdf/rdf_list.rb
@@ -1,17 +1,49 @@
-# This is a VERY temporary workaround to enable basic lists. Awaiting @tjohnson for something better.
-module ActiveFedora::Rdf::RdfList
-  extend ActiveSupport::Concern
-  included do
-    def [](index)
-      self.send(self.class.list)[index]
+module ActiveFedora::Rdf
+  class RdfList < RDF::List
+
+    def initialize(*args)
+      super
+      @graph = ListResource.new(subject) << graph
     end
-  end
-  module ClassMethods
-    def list
-      @list
+
+    def resource
+      graph
     end
-    def list=(value)
-      @list = value
+    ##
+    # Monkey patch to allow lists to have subject URIs.
+    # Overrides shift in RDF::List to prevent URI subjects
+    # from being replaced with nodes.
+    #
+    # @NOTE Lists built this way will return false for #valid?
+    def <<(value)
+      value = case value
+              when nil         then RDF.nil
+              when RDF::Value  then value
+              when Array       then RDF::List.new(nil, graph, value)
+              else value
+              end
+
+      if empty?
+        @subject = RDF::Node.new if @subject == RDF.nil
+        new_subject = subject
+      else
+        old_subject, new_subject = last_subject, RDF::Node.new
+        graph.delete([old_subject, RDF.rest, RDF.nil])
+        graph.insert([old_subject, RDF.rest, new_subject])
+      end
+
+      graph.insert([new_subject, RDF.first, value.is_a?(RDF::List) ? value.subject : value])
+      graph.insert([new_subject, RDF.rest, RDF.nil])
+
+      self
+    end
+
+    def self.from_uri(uri, vals=nil)
+      list = ListResource.from_uri(uri, vals)
+      self.new(list.rdf_subject, list)
+    end
+
+    class ListResource < RdfResource
     end
   end
 end
