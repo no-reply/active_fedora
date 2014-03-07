05898ecf8b97c06924f67b5fffb70a0f3d97299a
Tom Johnson <thomas.johnson@oregonstate.edu>
2014-03-06 17:25:05 -0800

    making resource datastream on Rdf::Identifiable configurable

--- lib/active_fedora/rdf/identifiable.rb
+++ lib/active_fedora/rdf/identifiable.rb
@@ -18,11 +18,20 @@ module ActiveFedora::Rdf::Identifiable
     self.send(self.class.resource_datastream).resource
   end
   module ClassMethods
-    def resource_datastream
+    ##
+    # Returns the datastream whose ActiveFedora::Rdf::Resource is used
+    # for the identifiable object. This supports delegating methods to
+    # the Resource and #from_uri.  Defaults to :descMetadata if it
+    # responds to #rdf_subject, otherwise looks for the first
+    # registered datastream that does.
+    def resource_datastream(ds=nil)
+      @resource_datastream ||= nil
+      @resource_datastream = ds if ds
+      return @resource_datastream if @resource_datastream
+      return :descMetadata if self.ds_specs[:descMetadata][:type].respond_to? :rdf_subject
       self.ds_specs.each do |dsid, conf|
         return dsid.to_sym if conf[:type].respond_to? :rdf_subject
       end
-      return :descMetadata
     end
     ##
     # Finds the appropriate ActiveFedora::Base object given a URI from a graph.
